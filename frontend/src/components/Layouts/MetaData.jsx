9c6ed6dd1e732f63af146c6b80d3e7c36fc4f2e5
PROJECT <nishadgaurav2017@gmail.com>
2025-03-25 18:04:49 +0530

    update

---
+++ frontend/src/components/Layouts/MetaData.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+
+const MetaData = ({ title }) => {
+    return (
+        // <Helmet>
+        //     <title>{title}</title>
+        // </Helmet>
+        <>
+            <title>{title}</title>
+        </>
+    );
+};
+
+export default MetaData;
